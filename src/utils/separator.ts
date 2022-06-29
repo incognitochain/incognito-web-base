ec8e2fc9a7bab8c54e70980a4daff98c1fd06fea
Sang Incognito <72240121+matterhorn063@users.noreply.github.com>
2022-06-29 17:29:51 +0700

    update code

---
+++ src/utils/separator.ts
@@ -0,0 +1,15 @@
+const decimalSeparator = '.';
+const groupSeparator = ',';
+
+export function getDecimalSeparator() {
+  return decimalSeparator;
+}
+
+export function getGroupSeparator() {
+  return groupSeparator;
+}
+
+export default {
+  getDecimalSeparator,
+  getGroupSeparator,
+};
