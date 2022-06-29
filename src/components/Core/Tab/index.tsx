31018ea2a7d64acb7fd11623cf4f526283a5b7f2
Sang Incognito <72240121+matterhorn063@users.noreply.github.com>
2022-06-29 14:28:46 +0700

    update code

--- src/components/Core/Tab/index.tsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components/macro';
-
-const Row = styled.div``;
-
-export default Row;
---
+++ src/components/Core/Tabs/Tabs.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Tabs = React.memo(() => {
+  return <div />;
+});
+
+Tabs.displayName = 'Tabs';
+
+export default Tabs;
