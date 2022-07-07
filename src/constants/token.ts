f02847abebdd6c0fc5b783e8c82a43c3afcba743
Sang Incognito <72240121+matterhorn063@users.noreply.github.com>
2022-07-07 19:24:03 +0700

    add logo

--- src/constants/token.ts
+++ src/constants/token.ts
@@ -1,4 +1,5 @@
 import ethereumLogoUrl from 'assets/images/ethereum-logo.png';
+import ftmLogoUrl from 'assets/images/ftm_logo.png';
 import bnbChainLogo from 'assets/svg/bnbchain-logo.svg';
 import optimismLogoUrl from 'assets/svg/optimistic_ethereum.svg';
 import polygonMaticLogo from 'assets/svg/polygon-matic-logo.svg';
@@ -130,9 +131,9 @@ const ROOT_NETWORK_IMG: any = {
   [PRIVATE_TOKEN_CURRENCY_TYPE.POLYGON_ERC20]: polygonMaticLogo,
 
   [PRIVATE_TOKEN_CURRENCY_TYPE.FTM]: optimismLogoUrl,
-  [PRIVATE_TOKEN_CURRENCY_TYPE.FANTOM_ERC20]: optimismLogoUrl,
+  [PRIVATE_TOKEN_CURRENCY_TYPE.FANTOM_ERC20]: ftmLogoUrl,
 
-  [PRIVATE_TOKEN_CURRENCY_TYPE.UNIFIED_TOKEN]: optimismLogoUrl,
+  [PRIVATE_TOKEN_CURRENCY_TYPE.UNIFIED_TOKEN]: ftmLogoUrl,
 };
 
 export {
