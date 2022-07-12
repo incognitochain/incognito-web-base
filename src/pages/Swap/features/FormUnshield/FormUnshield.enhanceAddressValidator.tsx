689f6a765f71185e64f00f601f71a41066b5104e
Sang Incognito <72240121+matterhorn063@users.noreply.github.com>
2022-07-12 12:02:23 +0700

    push code

---
+++ src/pages/Swap/features/FormUnshield/FormUnshield.enhanceAddressValidator.tsx
@@ -0,0 +1,44 @@
+import { validator } from 'components/Core/ReduxForm';
+import { FORM_CONFIGS } from 'pages/Swap/Swap.constant';
+import React from 'react';
+
+export interface TInner {
+  validateAddress: () => any;
+  warningAddress: () => string;
+}
+
+const enhanceAddressValidation = (WrappedComponent: any) => {
+  const FormDepositComp = (props: any) => {
+    const { isIncognitoAddress, onChangeField, incAccount } = props;
+    const getExternalAddressValidator = React.useCallback(() => {
+      // default
+      return validator.combinedUnknownAddress;
+    }, [isIncognitoAddress]);
+
+    const getAddressValidator = React.useCallback(() => {
+      return validator.combinedIncognitoAddress;
+    }, [isIncognitoAddress]);
+
+    const getWarningAddress = React.useCallback(() => {
+      // if (isExternalAddress) {
+      //   return 'You are exiting Incognito and going public.';
+      // }
+      // return "You are exiting Incognito and going public.";
+    }, []);
+
+    const validateAddress = getAddressValidator();
+    const warningAddress = getWarningAddress();
+
+    React.useEffect(() => {
+      if (incAccount && incAccount.paymentAddress && onChangeField) {
+        onChangeField(incAccount.paymentAddress, FORM_CONFIGS.toAddress);
+      }
+    }, [incAccount]);
+
+    return <WrappedComponent {...{ ...props, validateAddress, warningAddress }} />;
+  };
+  FormDepositComp.displayName = 'FormDeposit.enhanceAddressValidation';
+  return FormDepositComp;
+};
+
+export default enhanceAddressValidation;
