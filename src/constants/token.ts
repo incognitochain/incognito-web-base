66ef95081308fb8a44ce0560c453db3f6340cd9d
Sang Incognito <72240121+matterhorn063@users.noreply.github.com>
2022-07-12 11:35:29 +0700

    fix bug

--- src/constants/token.ts
+++ src/constants/token.ts
@@ -2,7 +2,6 @@ import ethereumLogoUrl from 'assets/images/ethereum-logo.png';
 import ftmLogoUrl from 'assets/images/ftm_logo.png';
 import incognitoLogoUrl from 'assets/images/inc_logo.png';
 import bnbChainLogo from 'assets/svg/bnbchain-logo.svg';
-import optimismLogoUrl from 'assets/svg/optimistic_ethereum.svg';
 import polygonMaticLogo from 'assets/svg/polygon-matic-logo.svg';
 import { isMainnet } from 'config';
 
@@ -132,7 +131,7 @@ const ROOT_NETWORK_IMG: any = {
   [PRIVATE_TOKEN_CURRENCY_TYPE.MATIC]: polygonMaticLogo,
   [PRIVATE_TOKEN_CURRENCY_TYPE.POLYGON_ERC20]: polygonMaticLogo,
 
-  [PRIVATE_TOKEN_CURRENCY_TYPE.FTM]: optimismLogoUrl,
+  [PRIVATE_TOKEN_CURRENCY_TYPE.FTM]: ftmLogoUrl,
   [PRIVATE_TOKEN_CURRENCY_TYPE.FANTOM_ERC20]: ftmLogoUrl,
 
   [PRIVATE_TOKEN_CURRENCY_TYPE.UNIFIED_TOKEN]: incognitoLogoUrl,
