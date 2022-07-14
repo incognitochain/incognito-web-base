e617744b5b5fea6e5d21520fcda962dc3b7d2940
Sang Incognito <72240121+matterhorn063@users.noreply.github.com>
2022-07-14 18:10:50 +0700

    push code

--- src/components/Modal/balance/FollowTokens/FollowTokens.token.tsx
+++ src/components/Modal/balance/FollowTokens/FollowTokens.token.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/display-name */
+import SelectedPrivacy from 'models/model/SelectedPrivacyModel';
 import React from 'react';
 import { useAppSelector } from 'state/hooks';
 import { getPrivacyDataByTokenIDSelector } from 'state/token';
@@ -7,9 +8,9 @@ import styled from 'styled-components/macro';
 const Styled = styled.div``;
 
 const Token = React.memo(({ tokenID }: { tokenID: string }) => {
-  const selectedToken = useAppSelector(getPrivacyDataByTokenIDSelector)(tokenID);
+  const selectedToken: SelectedPrivacy = useAppSelector(getPrivacyDataByTokenIDSelector)(tokenID);
   if (!selectedToken.symbol) return null;
-  return <Styled>{selectedToken.formatAmountNoClip}</Styled>;
+  return <Styled>{`${selectedToken.formatAmount} ${selectedToken.symbol}`}</Styled>;
 });
 
 export default Token;
