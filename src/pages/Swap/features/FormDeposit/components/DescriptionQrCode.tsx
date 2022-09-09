79030a1cb9b8b2c289c6082df93ed738e3b40509
Bui Tan Phat <phatbt@incognito.org>
2022-09-09 09:59:59 +0700

    Modify code

--- src/pages/Swap/features/FormDeposit/components/DescriptionQrCode.tsx
+++ src/pages/Swap/features/FormDeposit/components/DescriptionQrCode.tsx
@@ -1,19 +1,36 @@
 import CopyIcon from 'components/Copy/index';
 import styled from 'styled-components/macro';
 import { ThemedText } from 'theme';
+import { ellipsisCenter } from 'utils';
 
 const Styled = styled.div`
   margin-top: 16px;
   flex: 1;
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
-
-  .paymentAddress-area {
+  .center {
     display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .paymentAddress-area {
+    margin-top: 10px;
+    display: flex;
+    flex: 1;
+    padding: 14px;
     flex-direction: row;
     align-items: center;
+    overflow: hidden;
+    height: 54px;
+    border-radius: 8px;
+    border: 1px solid ${({ theme }) => theme.border1};
+    background-color: ${({ theme }) => theme.primary14};
+
+    .paymentAddressText {
+      width: 85%;
+      margin-right: 20px;
+    }
   }
 `;
 
@@ -26,14 +43,23 @@ interface DescriptionQrCodeProps {
 const DescriptionQrCode = ({ symbol, paymentAddress }: DescriptionQrCodeProps) => {
   return (
     <Styled>
-      <ThemedText.SmallLabel fontWeight={400} color="primary8">
-        {`Send only ${symbol} to this shielding address`}
-      </ThemedText.SmallLabel>
+      <div className="center">
+        <ThemedText.SmallLabel fontWeight={400} color="primary8">
+          {`Send only ${symbol} to this shielding address`}
+        </ThemedText.SmallLabel>
+      </div>
+
       {paymentAddress && (
         <div className="paymentAddress-area">
-          <ThemedText.SmallLabel fontWeight={500} color="primary5">
-            {paymentAddress}
-          </ThemedText.SmallLabel>
+          <div className="paymentAddressText">
+            <ThemedText.SmallLabel fontWeight={500} color="primary5">
+              {ellipsisCenter({
+                str: paymentAddress || '',
+                limit: 18,
+              })}
+            </ThemedText.SmallLabel>
+          </div>
+
           <CopyIcon text={paymentAddress} />
         </div>
       )}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -61,3 +61,24 @@ export function shortenIncognitoAddress(address: string, chars = 6): string {
   }
   return `${address.substring(0, chars)}...${address.substring(address.length - chars)}`;
 }
+
+interface IEllipsis {
+  str: string;
+  limit?: number;
+  dots?: string;
+}
+
+export const ellipsisCenter = (payload: IEllipsis) => {
+  const { str, limit = 10, dots = '...' }: IEllipsis = payload;
+  try {
+    const size = str.length;
+    if (size < limit * 2 + dots.length) {
+      return str;
+    }
+    const leftStr = str.substring(0, limit);
+    const rightStr = str.substring(size - limit, size);
+    return leftStr + dots + rightStr;
+  } catch {
+    return str;
+  }
+};
