db49f19a08b12fa2c5f13121f1c109dec7852334
SangNX <sang@incognito.org>
2022-12-21 16:27:47 +0700

    Update UI

--- src/pages/GetPRV/GetPRV.tsx
+++ src/pages/GetPRV/GetPRV.tsx
@@ -2,13 +2,36 @@ import { Col } from 'antd';
 import { PRVIcon } from 'components/icons';
 import React, { memo } from 'react';
 
+import { useWindowSize } from '../../hooks/useWindowSize';
+import { FOOTER_ID, HEADER_ID } from '../App';
 import Analytics from './GetPRV.analytics';
 import Promote from './GetPRV.promote';
 import { Container, Content, Header } from './GetPRV.styled';
 
 const GetPRV = () => {
+  const { width, height } = useWindowSize();
+
+  const [contentSize, setContentSize] = React.useState(height || 0);
+
+  const getContentSize = () => {
+    let contentSize = 0;
+    const header = document.getElementById(HEADER_ID);
+    const footer = document.getElementById(FOOTER_ID);
+    if (height && header && footer) {
+      const headerHeight = header.clientHeight;
+      // const footerHeight = footer.clientHeight;
+      const footerHeight = 0;
+      contentSize = height - headerHeight - footerHeight - 35;
+    }
+    setContentSize(contentSize);
+  };
+
+  React.useEffect(() => {
+    getContentSize();
+  }, [width, height]);
+
   return (
-    <Container className="default-max-width">
+    <Container className="default-max-width" height={contentSize}>
       <Header>
         <Col className="col-1">
           <Col>
