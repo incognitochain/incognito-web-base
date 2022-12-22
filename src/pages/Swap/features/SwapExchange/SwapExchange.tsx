431df6de6a5fad364a3eacf906a6be7f274b31e9
SangNX <sang@incognito.org>
2022-12-22 18:42:14 +0700

    modify code

--- src/pages/Swap/features/SwapExchange/SwapExchange.tsx
+++ src/pages/Swap/features/SwapExchange/SwapExchange.tsx
@@ -1,26 +1,20 @@
 import Loader from 'components/Core/Loader';
 import { TAB_LIST, Tabs } from 'components/Core/Tabs';
-import { selectedTabSelector } from 'components/Core/Tabs/Tabs.selectors';
 import { FormUnshield } from 'pages/Swap/features/FormUnshield';
 import enhance from 'pages/Swap/Swap.enhance';
+import { getQueryPAppName } from 'pages/Swap/Swap.hooks';
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from 'state/hooks';
+import { useAppSelector } from 'state/hooks';
 import { isFetchingSelectors as isFetchingTokenSelector } from 'state/token';
 
-import { getQueryPAppName } from '../../Swap.hooks';
 import { capitalizeFirstLetter } from '../ItemDetail/ItemDetail';
 import { Container, Content } from './SwapExchange.styled';
 
 const SwapExchange = (props: any) => {
   let { SWAP_PAPP: HEADER_TAB } = TAB_LIST;
-  const selectedTab = useAppSelector(selectedTabSelector)(HEADER_TAB.rootTab);
   const isFetching = useAppSelector(isFetchingTokenSelector);
-  const dispatch = useAppDispatch();
-  const location: any = useLocation();
 
   const renderForm = () => {
-    // if (selectedTab === HEADER_TAB.tabNames[1]) return <FormDeposit {...props} />;
     return <FormUnshield {...props} />;
   };
 
