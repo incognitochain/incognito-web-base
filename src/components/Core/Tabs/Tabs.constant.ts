92e54c9601da5ef95192668dbd20f1b788af7013
SangNX <sang@incognito.org>
2022-12-23 10:46:15 +0700

    update ui

--- src/components/Core/Tabs/Tabs.constant.ts
+++ src/components/Core/Tabs/Tabs.constant.ts
@@ -12,10 +12,6 @@ const TAB_LIST: ITabList = {
     tabNames: ['Swap', 'Deposit'],
     rootTab: getRootTab('SWAP'),
   },
-  SWAP_PAPP: {
-    tabNames: [],
-    rootTab: getRootTab('SWAP_PAPP'),
-  },
   INCOGNITO_ACCOUNT: {
     // tabNames: ['Profile', 'Submit Proof', 'Transactions'],
     tabNames: ['Profile', 'Transactions'],
--- src/pages/Swap/features/SwapExchange/SwapExchange.tsx
+++ src/pages/Swap/features/SwapExchange/SwapExchange.tsx
@@ -1,5 +1,4 @@
 import Loader from 'components/Core/Loader';
-import { TAB_LIST, Tabs } from 'components/Core/Tabs';
 import { FormUnshield } from 'pages/Swap/features/FormUnshield';
 import enhance from 'pages/Swap/Swap.enhance';
 import { getQueryPAppName } from 'pages/Swap/Swap.hooks';
@@ -11,7 +10,6 @@ import { capitalizeFirstLetter } from '../ItemDetail/ItemDetail';
 import { Container, Content } from './SwapExchange.styled';
 
 const SwapExchange = (props: any) => {
-  let { SWAP_PAPP: HEADER_TAB } = TAB_LIST;
   const isFetching = useAppSelector(isFetchingTokenSelector);
 
   const renderForm = () => {
@@ -20,12 +18,12 @@ const SwapExchange = (props: any) => {
 
   const renderContent = () => {
     const query = getQueryPAppName();
-    if (query.isValid && query.pAppName) {
-      HEADER_TAB = { ...HEADER_TAB, tabNames: [`p${capitalizeFirstLetter(query.pAppName)}`] };
-    }
     return (
       <>
-        <Tabs {...HEADER_TAB} />
+        <p
+          className="h7"
+          style={{ color: 'white', fontWeight: 600, fontSize: 20, lineHeight: '140%' }}
+        >{`p${capitalizeFirstLetter(query?.pAppName || '')}`}</p>
         {renderForm()}
       </>
     );
