34b23c288c4c67bb4a059e7902e936ff2333416e
TN <thu@incognito.org>
2022-12-29 19:37:59 +0700

    fix bug

--- src/pages/Dao/ProposalDetail/components/HeaderBpx.tsx
+++ src/pages/Dao/ProposalDetail/components/HeaderBpx.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/prop-types */
 import { Skeleton } from 'antd';
 import BackButton from 'components/BackButton';
+import { ButtonConfirmed } from 'components/Core/Button';
 import ProposalStatusBox from 'pages/Dao/Governance/components/ProposalStatus';
 import { Proposal } from 'state/dao/types';
-import styled from 'styled-components/macro';
+import styled, { DefaultTheme } from 'styled-components/macro';
 
 interface HeaderProps {
   proposal?: Proposal;
   isLoading?: boolean;
+  isDisabledButtonVote?: boolean;
+  onClickVoteButton?: () => void;
 }
 
 const ProposalTitle = styled.p`
@@ -16,11 +19,50 @@ const ProposalTitle = styled.p`
   line-height: 140%;
   color: #ffffff;
   margin-top: 8px;
+  flex: 1;
+  margin-right: 36px;
+`;
+
+const ProposalIdText = styled.p`
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 140%;
+  color: #757575;
+  margin-left: 16px;
+
+  ${({ theme }: { theme: DefaultTheme }) => theme.mediaWidth.upToSmall`
+    margin-right: 0px;
+  `}
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  ${({ theme }: { theme: DefaultTheme }) => theme.mediaWidth.upToSmall`
+    flex-direction: column;
+    align-items: flex-start;
+  `}
+`;
+
+const AddProposalButtonContainer = styled.div`
+  ${({ theme }: { theme: DefaultTheme }) => theme.mediaWidth.upToSmall`
+    width: 100%;
+    margin-top: 24px;
+  `}
+`;
+
+const AddProposalButton = styled(ButtonConfirmed)`
+  ${({ theme }: { theme: DefaultTheme }) => theme.mediaWidth.upToSmall`
+    width: 100%
+  `}
 `;
 
 const HeaderBox: React.FC<HeaderProps> = (props) => {
   // eslint-disable-next-line react/prop-types
-  const { proposal, isLoading } = props;
+  const { proposal, isLoading, isDisabledButtonVote, onClickVoteButton } = props;
   const { title } = proposal || {};
 
   if (isLoading) {
@@ -34,17 +76,25 @@ const HeaderBox: React.FC<HeaderProps> = (props) => {
   }
 
   return (
-    <div style={{ marginBottom: 40, width: '100%', flexDirection: 'row', alignItems: 'center' }}>
+    <div style={{ marginBottom: 40, width: '100%' }}>
       <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
         <div>
           <BackButton />
         </div>
+        <ProposalIdText>Proposal 63ac61007a2bf687f0a611cf</ProposalIdText>
         <div style={{ maxWidth: 140 }}>
           <ProposalStatusBox status={proposal?.status} />
         </div>
       </div>
 
-      <ProposalTitle>{title}</ProposalTitle>
+      <TitleContainer>
+        <ProposalTitle>{title}</ProposalTitle>
+        <AddProposalButtonContainer>
+          <AddProposalButton disabled={isDisabledButtonVote} onClick={() => onClickVoteButton?.()} height={'40px'}>
+            Submit vote
+          </AddProposalButton>
+        </AddProposalButtonContainer>
+      </TitleContainer>
     </div>
   );
 };
