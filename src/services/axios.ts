57f3c2e859ff72728b054278a1207d6f33d07b90
Thu Ngo <thu@incognito.org>
2023-01-10 12:13:33 +0700

    revert code

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -19,7 +19,6 @@ const createAxiosInstance = ({ baseURL = '' }: { baseURL: string }) => {
     baseURL,
     timeout: TIMEOUT,
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ3YWxsZXRBZGRyZXNzIjoiMHgxYzZhMWZlY2M0NDQ5ZjBmN2M5MDQ3MTlhNjU0MDhmYWQ3ZmUzOTJhIiwic2lnbmF0dXJlIjoiMHg3ZDM4MTUwMjY5ZGRhMDM0ZmQ0OGVmZTY0YjI3Mjc1YTNiMWNlYTRhOTY5ODU5ZjBjYTQ1MjJjMzFlNjMzYzdhNjRlNzRlNDJjOTNjZGYzZGJiYTMwNjJiNmQ5NGUwMjkzODkyOTJkOWYxMTQzZWIzODM0NDUwOGEzZjQxNjBiYTFjIiwiaWF0IjoxNjcyOTk2MjU1LCJleHAiOjE2NzU1ODgyNTV9.7cxDRHBslEJoqYC5raVv8lid66mjrmHpTCutAbBVkb4`,
       ...HEADERS,
     },
   });
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,3 @@
 export { default as rpcClient } from './rpcClient';
 export { default as rpcExplorer } from './rpcExplorer';
-export { default as rpcPBlur } from './rpcPBlur';
 export { default as rpcTxService } from './rpcTxService';
