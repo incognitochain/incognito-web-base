e4676c2aa3c73a8c63b3b0b7d30edc5f54351702
Quoc279 <quoc.nguyen@autonomous.nyc>
2023-02-02 13:14:27 +0700

    Update

--- src/pages/Pnft/features/Collections/Collections.tsx
+++ src/pages/Pnft/features/Collections/Collections.tsx
@@ -2,6 +2,7 @@ import Content from './Collections.content';
 import PnftInfo from './Collections.info';
 import { Container } from './Collections.styled';
 
+// Collections
 const Collections = () => {
   return (
     <Container>
