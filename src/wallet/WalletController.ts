41c9730769bbd6735c6e30eada154b7496d53721
Thu Ngo <thu@incognito.org>
2023-03-10 14:02:07 +0700

    update create tx swap

--- src/wallet/WalletController.ts
+++ src/wallet/WalletController.ts
@@ -1,4 +1,8 @@
+import store from 'state';
+import { defaultAccountWalletSelector } from 'state/account/account.selectors';
+
 import { WalletType } from './types';
+const { createNewCoins } = require('incognito-chain-web-js/build/web/wallet');
 
 class WalletController {
   private walletType: WalletType = 'WalletWeb';
@@ -31,6 +35,67 @@ class WalletController {
 
   get isWalletWeb(): boolean {
     return !!(this.walletType === 'WalletWeb');
+  }
+
+  async requestAccount(req: any) {
+    let resp: any = {};
+    let result: any = {};
+    try {
+      const accountSender = defaultAccountWalletSelector(store.getState());
+      const otaReceiver = await accountSender?.getOTAReceive();
+      const burnerAddress = await accountSender?.getBurnerAddress2();
+      let otaReceiverWithCfg;
+      if (req.params && req.params?.senderShardID !== undefined) {
+        otaReceiverWithCfg = await accountSender?.getOTAReceiveWithCfg({
+          senderShardID: req.params?.senderShardID,
+        });
+      }
+      result.otaReceiver = otaReceiver;
+      result.burnerAddress = burnerAddress;
+      result.otaReceiverWithCfg = otaReceiverWithCfg;
+      const paymentAddress = accountSender.getPaymentAddress();
+      result.newCoins = await createNewCoins(paymentAddress);
+      resp.result = result;
+      return resp;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async signTransaction(req: any) {
+    const { isSignAndSendTransaction, fee, tokenID, txType, version, prvPayments, tokenPayments, metadata, info } = req;
+
+    let tx: any;
+    try {
+      const accountSender = defaultAccountWalletSelector(store.getState());
+      const params = {
+        fee,
+        tokenID,
+        txType,
+        version,
+        prvPayments,
+        tokenPayments,
+        metadata,
+        info,
+      };
+      let _tokenPayments = tokenPayments;
+      if (!_tokenPayments || _tokenPayments.length === 0) {
+        _tokenPayments = null;
+      }
+      if (isSignAndSendTransaction) {
+        tx = await accountSender.createAndSignTransaction({ ...params, tokenPayments: _tokenPayments });
+      } else {
+        tx = await accountSender.createTransaction({ ...params, tokenPayments: _tokenPayments });
+      }
+    } catch (error) {
+      console.log('CREATE TRANSACTION ERROR: ', error);
+    }
+
+    if (tx && (tx.hash || tx.txId)) {
+      return { txHash: tx.hash || tx.txId, rawData: tx.rawTx || tx.rawData };
+    } else {
+      return new Error('Create Transaction failed');
+    }
   }
 }
 
