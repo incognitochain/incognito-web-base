2ba0cc85944a0c21424ab9581aa653e8cb4d7eab
Bui Tan Phat <phatbt@incognito.org>
2023-04-06 18:03:00 +0700

    hotfix: modify layout and enhance validate pw and confirm pw

--- src/pages/IncWebWallet/features/ImportWallet/ImportWallet.styled.tsx
+++ src/pages/IncWebWallet/features/ImportWallet/ImportWallet.styled.tsx
@@ -2,7 +2,7 @@ import styled, { DefaultTheme } from 'styled-components/macro';
 
 export const Styled = styled.div`
   width: 100%;
-
+  margin-bottom: 70px;
   ${({ theme }: { theme: DefaultTheme }) => theme.mediaWidth.upToLarge`
       
   `}
