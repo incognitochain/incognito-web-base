6a09d040f291b0b8ced90d7c43334e992278f4eb
Bui Tan Phat <phatbt@incognito.org>
2023-04-07 11:37:16 +0700

    Modify Layout, Add description when create Password

--- src/pages/IncWebWallet/features/CreateWallet/CreateWallet.styled.tsx
+++ src/pages/IncWebWallet/features/CreateWallet/CreateWallet.styled.tsx
@@ -1,12 +1,10 @@
 import styled, { DefaultTheme } from 'styled-components/macro';
 
-export const Styled = styled.div`
+export const Styled = styled.div<{ height: number }>`
   width: 100%;
-  height: 80vh;
+  min-height: ${({ height }) => height}px;
   ${({ theme }: { theme: DefaultTheme }) => theme.mediaWidth.upToLarge`
-       height: 80vh;
   `}
   ${({ theme }: { theme: DefaultTheme }) => theme.mediaWidth.upToMedium`
-       height: 85vh;
   `}
 `;
