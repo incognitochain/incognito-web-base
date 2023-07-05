7b13cc3d5a3d451aa3919a476748fd216f4e46e6
Bui Tan Phat <phatbt@incognito.org>
2023-07-05 17:21:36 +0700

    Change privacy.wasm and modify code

--- src/pages/App.enhance.tsx
+++ src/pages/App.enhance.tsx
@@ -1,11 +1,9 @@
-import axios from 'axios';
 import ErrorBoundary from 'components/Core/ErrorBoundary';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { compose } from 'redux';
-import { actionGetPools, actionSetExplorer } from 'state/pools';
+import { actionGetPools } from 'state/pools';
 import { actionGetPTokens } from 'state/token';
-import { KEYS, StorageManager } from 'storage';
 
 import enhanceLoadWasm from './App.enhanceLoadWasm';
 import enhanceScainCoin from './App.enhanceScainCoin';
@@ -17,13 +15,13 @@ const enhance = (WrappedComponent: React.FunctionComponent) => {
     const getPTokenList = () => {
       dispatch(actionGetPTokens());
       try {
-        axios.get('https://api-explorer.incognito.org/api/v1/explorer/summary').then((data) => {
-          const volume = Math.ceil(
-            data.data.data.find((item: any) => item['metricType'] === 'TRADING_VOLUME_TOTAL').value / 1e6
-          );
-          StorageManager.setItem(KEYS.TRADE_VOLUME, `${volume}`);
-          dispatch(actionSetExplorer(data.data.data));
-        });
+        // axios.get('https://api-explorer.incognito.org/api/v1/explorer/summary').then((data) => {
+        //   const volume = Math.ceil(
+        //     data.data.data.find((item: any) => item['metricType'] === 'TRADING_VOLUME_TOTAL').value / 1e6
+        //   );
+        //   StorageManager.setItem(KEYS.TRADE_VOLUME, `${volume}`);
+        //   dispatch(actionSetExplorer(data.data.data));
+        // });
       } catch (e) {
         console.log('GET DATA ERROR', e);
       }
